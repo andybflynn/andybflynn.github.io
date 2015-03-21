ca120798ddedc49466bd82c06d5caac61850644d
Flynn, Andy <Andrew.Flynn@specialistholidays.com>
2015-03-21 21:51:02 +0000

    Preventing default scroll behaviour on smoothscroll

--- scripts/main.js
+++ scripts/main.js
@@ -22,7 +22,8 @@ var Global = {
 						matchedEls.height(maxHeight);
 					});
 	}
-	, smoothScrollTo: function($target, offsetPercentage) {
+	, smoothScrollTo: function(e, $target, offsetPercentage) {
+		e.preventDefault();
 		if ($target.length > 0) {
 			$('html,body').animate({
 	          scrollTop: $target.offset().top + ($target.height()*(offsetPercentage/100))
@@ -33,13 +34,13 @@ var Global = {
 $(document).ready(function() {
 	//call any methods that need to be run on DOM ready
 	Global.matchHeights();
-	$('[data-smooth-scroll]').on('click', function() {
+	$('[data-smooth-scroll]').on('click', function(e) {
 		var $smoothScrollEl = $(this)
 			, $target = $smoothScrollEl.data('smooth-scroll-target') || $smoothScrollEl
 			, offsetPercentage = $smoothScrollEl.data('smooth-scroll-offset-percentage') || 0
 		;
 
-		Global.smoothScrollTo($($target), offsetPercentage);
+		Global.smoothScrollTo(e, $($target), offsetPercentage);
 	});
 	
 });
