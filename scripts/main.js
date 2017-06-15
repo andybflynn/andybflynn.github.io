9430eff39ea0c7032d19b567122689e29e942095
Andy Flynn <andybflynn@gmail.com>
2017-06-15 22:27:22 +0100

    Reintroducing match heights JS for proper height matching

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,28 @@
 //register global namespace and global functions
 
 var Global = {
-	smoothScrollTo: function(e, $target, offsetPercentage) {
+	matchHeights: function() {
+					//look for all items with height match data attributes
+					$('[data-match-heights]').each(function() {
+						var $this = $(this)
+							, innerClass = $this.data('match-heights')
+							, maxHeight = 0
+							, matchedEls = $this.find(innerClass)
+						;
+
+						//loop through the inner classes to find the match height, then set all of them to the max
+						matchedEls.each(function() {
+							//remove any existing inline height
+							$(this).css('height','');
+							var height = $(this).height();
+							if (height > maxHeight) {
+								maxHeight = height;
+							}
+						});
+						matchedEls.height(maxHeight);
+					});
+	}
+	, smoothScrollTo: function(e, $target, offsetPercentage) {
 		e.preventDefault();
 		if ($target.length > 0) {
 			$('html,body').animate({
@@ -12,6 +33,7 @@ var Global = {
 }
 $(document).ready(function() {
 	//call any methods that need to be run on DOM ready
+	Global.matchHeights();
 	$('[data-smooth-scroll]').on('click', function(e) {
 		var $smoothScrollEl = $(this)
 			, $target = $smoothScrollEl.data('smooth-scroll-target') || $smoothScrollEl
@@ -21,4 +43,9 @@ $(document).ready(function() {
 		Global.smoothScrollTo(e, $($target), offsetPercentage);
 	});
 
+});
+
+$(window).on('resize', function() {
+	//call any methods that need to be run when the window is resized
+	Global.matchHeights();
 });
