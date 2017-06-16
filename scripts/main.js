0cb500f19d613c5029693390e45879689f541547
Andy Flynn <andybflynn@gmail.com>
2017-06-16 23:20:30 +0100

    Removing match heights js, centering card headings

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,7 @@
 //register global namespace and global functions
 
 var Global = {
-	matchHeights: function() {
-					//look for all items with height match data attributes
-					$('[data-match-heights]').each(function() {
-						var $this = $(this)
-							, innerClass = $this.data('match-heights')
-							, maxHeight = 0
-							, matchedEls = $this.find(innerClass)
-						;
-
-						//loop through the inner classes to find the match height, then set all of them to the max
-						matchedEls.each(function() {
-							//remove any existing inline height
-							$(this).css('height','');
-							var height = $(this).height();
-							if (height > maxHeight) {
-								maxHeight = height;
-							}
-						});
-						matchedEls.height(maxHeight);
-					});
-	}
-	, smoothScrollTo: function(e, $target, offsetPercentage) {
+	smoothScrollTo: function(e, $target, offsetPercentage) {
 		e.preventDefault();
 		if ($target.length > 0) {
 			$('html,body').animate({
@@ -33,7 +12,6 @@ var Global = {
 }
 $(document).ready(function() {
 	//call any methods that need to be run on DOM ready
-	Global.matchHeights();
 	$('[data-smooth-scroll]').on('click', function(e) {
 		var $smoothScrollEl = $(this)
 			, $target = $smoothScrollEl.data('smooth-scroll-target') || $smoothScrollEl
@@ -43,9 +21,4 @@ $(document).ready(function() {
 		Global.smoothScrollTo(e, $($target), offsetPercentage);
 	});
 
-});
-
-$(window).on('resize', function() {
-	//call any methods that need to be run when the window is resized
-	Global.matchHeights();
 });
