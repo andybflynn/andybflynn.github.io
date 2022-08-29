6fcac05aa3a95e0b7fc2fb191a0bc8c0b60544fc
Andy Flynn <andy.flynn@boughtbymany.com>
2022-08-29 09:59:53 +0100

    copy calculator to docs

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,23 @@
 import { fileURLToPath, URL } from "node:url";
+import copy from "rollup-plugin-copy";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    copy({
+      hook: "writeBundle",
+      targets: [
+        {
+          src: "./portfolio/**/*",
+          dest: "./docs/portfolio",
+        },
+      ],
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
